34696c3c03d4267172fd401da4a616ebba727976
flavws <persona.fmaria@gmail.com>
2022-06-13 15:29:04 -0300

    Interação login e registro.

--- ColetaFacil/src/main/java/br/com/coletafacil/ColetaFacil/service/UserService.java
+++ ColetaFacil/src/main/java/br/com/coletafacil/ColetaFacil/service/UserService.java
@@ -1,9 +1,13 @@
 package br.com.coletafacil.ColetaFacil.service;
 
+import br.com.coletafacil.ColetaFacil.ColetaFacilApplication;
 import br.com.coletafacil.ColetaFacil.model.User;
 import br.com.coletafacil.ColetaFacil.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -15,4 +19,20 @@ public class UserService {
         return repository.save(user);
     }
 
+    public String loginUser(User user) {
+        List<User> users = repository.findAll();
+        for (User u: users) {
+            if (u.getUserEmail().equals(user.getUserEmail()) && u.getUserPassword().equals(user.getUserPassword())) {
+                return user.getUserEmail();
+            }
+        }
+
+        return "FAILURE";
+
+
+    }
+
+
 }
+
+
